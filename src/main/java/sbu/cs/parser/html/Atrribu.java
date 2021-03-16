f94a8661b7cfc6d3af2a7a85b8319f1f1c757fb1
HasanRoknabady <hasanrokn@yahoo.com>
2021-03-16 23:17:45 +0330

    html solve here

---
+++ src/main/java/sbu/cs/parser/html/Atrribu.java
@@ -0,0 +1,23 @@
+package sbu.cs.parser.html;
+
+class Attribu
+{
+    private String key;
+    private String value;
+
+    public Attribu(String value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+}
--- src/main/java/sbu/cs/parser/html/HTMLParser.java
+++ src/main/java/sbu/cs/parser/html/HTMLParser.java
@@ -1,22 +1,106 @@
 package sbu.cs.parser.html;
 
-public class HTMLParser {
 
-    /*
-    * this function will get a String and returns a Dom object
-     */
-    public static Node parse(String document) {
-        // TODO implement this
-        return null;
+public class HTMLParser
+{
+    public static Node parse(String document)
+    {
+        // d equal doucument with trim and delet \n
+        String d = document
+                           .replaceAll("\n", "")
+                                                                 .trim();
+
+        Node node = new Node(d);
+        d = removeTagName(d);
+
+        if(!d.contains("<"))
+        {
+            return node;
+        }
+
+        while(d.length() != 0)
+        {
+            node.addChild(firstTag(d));
+            d = removeTag(d);
+        }
+
+        return node;
     }
 
-    /*
-    * a function that will return string representation of dom object.
-    * only implement this after all other functions been implemented because this
-    * impl is not required for this series of exercises. this is for more score
-     */
-    public static String toHTMLString(Node root) {
-        // TODO implement this for more score
-        return null;
+
+
+     // we get html data here to see and ...
+    private static Node firstTag(String htmlData)
+    {
+        String tmp = htmlData.substring(htmlData.indexOf('<'), htmlData.indexOf('>') + 1);
+
+        if((htmlData.indexOf('<') + 1) == (htmlData.indexOf('/')))
+        {
+            return new Node(tmp.trim());
+        }
+        else
+        {
+            String openTag = tmp;
+
+            String tagName = htmlData
+                    .substring(htmlData.indexOf('<') + 1, Math.min(htmlData.indexOf('>'), htmlData.indexOf(' ')));
+
+            String closeTag = "</" + tagName + ">";
+
+            String tag = htmlData
+                    .substring(htmlData.indexOf(openTag) + openTag.length(), htmlData.indexOf(closeTag)).trim();
+
+            return new Node(openTag + tag + closeTag);
+        }
     }
+
+
+    private static String removeTag(String htmlData)
+    {
+        String temp = htmlData.substring(htmlData.indexOf('<'), htmlData.indexOf('>') + 1);
+
+        if((htmlData.indexOf('<') + 1) == (htmlData.indexOf('/')))
+        {
+            return htmlData.replaceFirst(temp, "").trim();
+        }
+        else
+        {
+            String openTag = temp;
+            String tagName = htmlData
+                                     .substring(htmlData.indexOf('<') + 1, Math.min(htmlData.indexOf('>'), htmlData.indexOf(' ')));
+            String closeTag = "</" + tagName + ">";
+            String tag = htmlData
+                                     .substring(htmlData.indexOf(openTag) + openTag.length(), htmlData.indexOf(closeTag));
+            String handle = htmlData.replaceFirst(openTag + tag + closeTag, "").trim();
+            return handle;
+        }
+
+    }
+
+
+
+    private static String removeTagName(String htmlData)
+    {
+        String openTag = htmlData
+                                 .substring(htmlData.indexOf('<'), htmlData.indexOf('>') + 1);
+
+        int temp = Math.min(htmlData.indexOf('>'), htmlData.indexOf(' '));
+        int idx;
+        if (temp < 0)
+        {
+            idx = htmlData.indexOf('>');
+        }
+        else
+        {
+            idx = temp;
+        }
+
+        String tagName = htmlData.substring(htmlData.indexOf('<') + 1, idx);
+        String closeTag = "</" + tagName + ">";
+        String handle = htmlData.replaceFirst(openTag, "").replaceFirst(closeTag, "");
+
+        return handle;
+    }
+
+
 }
